a6775cfde3cd610503677213d4917ca422e1ac6c
Add-rial <agastyagarg24@gmail.com>
2026-06-11 17:42:58 +0530

    Add resume_url column to users table in database schema

--- config/db.go
+++ config/db.go
@@ -52,7 +52,7 @@ func initSchema() error {
 			name VARCHAR(255) NOT NULL,
 			email VARCHAR(255) UNIQUE NOT NULL,
 			password VARCHAR(255) NOT NULL,
-			role VARCHAR(50) NOT NULL
+			role VARCHAR(50) NOT NULL,
 			resume_url TEXT
 		);`,
 		`CREATE TABLE IF NOT EXISTS jobs (
